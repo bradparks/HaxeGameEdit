0ca24b238b0df7dcab257955eede5ddb0e9430c8
yupswing <simonecingano@gmail.com>
2015-02-19 14:44:35 +0100

    Fix 'Nothing render in flash'
    Fix 'Nothing render in flash'
    (Mainly the MenuBar.hx)
    Cleaning around:
    - application.xml updated (better path control, few tricks)
    - .gitignore removed BIN folder and it will not sync anymore
    - fixed MenuBar.Text with getter and setter (otherwise it would not
    work as you, probably, were thinking)
    
    Cheers

--- src/Main.hx
+++ src/Main.hx
@@ -1,17 +1,23 @@
 package ;
-
+ 
 import openfl.display.Sprite;
 import openfl.events.Event;
-import openfl.Lib;
-
-class Main extends Sprite 
+ 
+class Main extends Sprite
 {
-	
-	public function new() 
-	{
-		super();	
-		
-		Lib.current.stage.addChild(new MainScene());
-		trace("Stage children: " + stage.numChildren);
-	}
+       
+        public function new()
+        {
+                super();
+                addEventListener(Event.ADDED_TO_STAGE, added);
+        }
+       
+        private function added(e:Event):Void
+        {
+                removeEventListener(Event.ADDED_TO_STAGE, added);
+                addChild(new MainScene());
+		//trace("Stage children: " + stage.numChildren);
+        }
+
+        
 }
--- src/MenuBar.hx
+++ src/MenuBar.hx
@@ -8,45 +8,60 @@ import openfl.Assets;
 import openfl.events.Event;
 import openfl.text.TextFormat;
 import openfl.text.TextFieldAutoSize;
+import openfl.Lib;
 
 class MenuBar extends Sprite
 {
 	private var _background:Bitmap;
 	private var _title:TextField;
-	public var Text:String;
 	private var _items:Array<MenuBarItem> = [];
+
+	// expose the TextField
+	public var Text(get, set):String;
+	private function get_Text() { 
+		return _title.text; 
+	}
+	private function set_Text(Text) { 
+		return _title.text = Text; 
+	}
 
 	public function new() 
 	{
 		super();
 		
-		addEventListener(Event.ADDED_TO_STAGE, init);
+		 //the init prepare everything
+		init();
+
+		// the constructor add to stage the stuff
+		addEventListener(Event.ADDED_TO_STAGE, constructor);
 	}
 	
-	private function init(e:Event):Void
+	private function init():Void
 	{
-		removeEventListener(Event.ADDED_TO_STAGE, init);
-		
-		_background = new Bitmap(Assets.getBitmapData("a/img/TopBar.png"));
+		_background = new Bitmap(Assets.getBitmapData("img/TopBar.png"));
 		_title = new TextField();
-		
-		_title.x = 0;
-		_title.y = 0;
 		_title.defaultTextFormat = Fonts.GetFormat("main-bold", 16, 0xFFFFFF);
-		_title.text = Text;
-		_title.embedFonts = true;
-		_title.selectable = false;
 		_title.autoSize = TextFieldAutoSize.LEFT;
-		_title.height = 48;
+		_title.embedFonts = true;
+		_title.selectable = true;
+		_title.wordWrap = false;
+		_title.border = false;
+		_title.x = 30;
+		_title.y = 15;
 		
-		_background.width = stage.stageWidth;
-		_background.height = 48;
+		_background.width = Lib.current.stage.stageWidth;
+		//_background.height = 48;
 		_background.x = 0;
 		_background.y = 0;
-		
+	}
+	
+	private function constructor(e:Event):Void
+	{
+		removeEventListener(Event.ADDED_TO_STAGE, constructor);
+
 		addChild(_background);
 		addChild(_title);
-		trace("Menu Bar children: " + numChildren);
+		//trace("Menu Bar children: " + numChildren);
 		
 		redraw();
 	}
